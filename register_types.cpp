9ee4756d5d011ed128a0bcdd8a96d1e39d5f97eb
George Marques <george@gmarqu.es>
2019-06-06 11:03:42 -0300

    Initial commit with Godot module structure

---
+++ register_types.cpp
@@ -0,0 +1,4 @@
+#include "register_types.h"
+
+void register_GodotPlayFab_types() {}
+void unregister_GodotPlayFab_types() {}
---
+++ register_types.h
@@ -0,0 +1,2 @@
+void register_GodotPlayFab_types();
+void unregister_GodotPlayFab_types();
