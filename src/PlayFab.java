d251b8495be5a524093f9afa6ff9fe84316964b6
George Marques <george@gmarqu.es>
2019-06-14 17:39:43 -0300

    Clean up module implementation
    Add functions to login with Android ID and link Facebook account.

--- src/PlayFab.java
+++ src/PlayFab.java
@@ -1,12 +1,13 @@
 package org.godotengine.godot;
 
 import android.app.Activity;
+import android.os.Build;
 import android.util.Log;
 
+import com.godot.game.BuildConfig;
 import com.playfab.PlayFabErrors.*;
 import com.playfab.PlayFabSettings;
 import com.playfab.PlayFabClientModels.*;
-import com.playfab.PlayFabClientAPI.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -18,8 +19,10 @@ import java.util.concurrent.FutureTask;
 
 import static com.playfab.PlayFabClientAPI.GetPlayerStatisticsAsync;
 import static com.playfab.PlayFabClientAPI.GetUserDataAsync;
+import static com.playfab.PlayFabClientAPI.LinkAndroidDeviceIDAsync;
+import static com.playfab.PlayFabClientAPI.LinkFacebookAccountAsync;
+import static com.playfab.PlayFabClientAPI.LoginWithAndroidDeviceIDAsync;
 import static com.playfab.PlayFabClientAPI.LoginWithFacebookAsync;
-import static com.playfab.PlayFabClientAPI.UpdatePlayerStatistics;
 import static com.playfab.PlayFabClientAPI.UpdatePlayerStatisticsAsync;
 import static com.playfab.PlayFabClientAPI.UpdateUserDataAsync;
 
@@ -36,7 +39,8 @@ public class PlayFab extends Godot.SingletonBase {
 
     public PlayFab(Activity p_activity) {
         registerClass("PlayFab", new String[]{
-                "login", "facebook_login", "init", "setUserData", "getUserData", "getPlayerStatistic", "setPlayerStatistic"
+                "login", "init", "setUserData", "getUserData", "getPlayerStatistic", "setPlayerStatistic",
+                "linkAndroidDeviceId", "linkFacebookAccount", "loginWithFacebook", "loginWithAndroidDeviceId"
         });
 
         activity = (Godot) p_activity;
@@ -44,57 +48,38 @@ public class PlayFab extends Godot.SingletonBase {
     }
 
     public void init(int instanceId, String titleId) {
-        this.instanceId = instanceId;
+        PlayFab.instanceId = instanceId;
         PlayFabSettings.TitleId = titleId;
     }
 
-    public void facebook_login(String access_token) {
+    public void loginWithFacebook(String accessToken) {
         LoginWithFacebookRequest request = new LoginWithFacebookRequest();
         request.CreateAccount = true;
-        request.AccessToken = access_token;
+        request.AccessToken = accessToken;
         request.TitleId = PlayFabSettings.TitleId;
 
-        FutureTask<PlayFabResult<LoginResult>> loginTask = LoginWithFacebookAsync(request);
-        loginTask.run();
-        Log.d("PlayFab", "Running facebook_login " + access_token);
-
-        while (_running) {
-            if (loginTask.isDone()) {
-                Log.d("PlayFab", "Login task done");
-                OnLoginComplete(loginTask);
+        treatResult(LoginWithFacebookAsync(request), "playfab_facebook_login_failed", new ResultRunnable<LoginResult>() {
+            @Override
+            public void run(LoginResult result) {
+                GodotLib.calldeferred(instanceId, "playfab_facebook_login_successful", new Object[]{});
             }
-
-            try {
-                Log.d("PlayFab", "Waiting login task");
-                Thread.sleep(1);
-            } catch (Exception e) {
-                Log.d("PlayFab", "Critical error in the example main loop: " + e);
-            }
-        }
-
-        _running = true;
+        });
     }
 
-    private static void OnLoginComplete(FutureTask<PlayFabResult<LoginResult>> loginTask) {
-        PlayFabResult<LoginResult> result = null;
-        try {
-            result = loginTask.get();
-        } catch (Exception e) {
-            Log.d("PlayFab", "Exception in PlayFab api call: " + e); // Did you assign your PlayFabSettings.TitleId correctly?
-        }
+    public void loginWithAndroidDeviceId(String androidId) {
+        LoginWithAndroidDeviceIDRequest request = new LoginWithAndroidDeviceIDRequest();
+        request.AndroidDeviceId = androidId;
+        request.AndroidDevice = Build.MODEL;
+        request.CreateAccount = true;
+        request.TitleId = PlayFabSettings.TitleId;
+        request.OS = Build.VERSION.RELEASE;
 
-        if (result != null && result.Result != null) {
-            GodotLib.calldeferred(instanceId, "playfab_login_successful", new Object[]{});
-        } else if (result != null && result.Error != null) {
-            String error = CompileErrorsFromResult(result);
-            Log.d("PlayFab", "Error when logging in. Here's some debug information:");
-            Log.d("PlayFab", error);
-            GodotLib.calldeferred(instanceId, "playfab_login_failed", new Object[]{error});
-        } else {
-            GodotLib.calldeferred(instanceId, "playfab_login_failed", new Object[]{"Unkown error"});
-        }
-
-        _running = false;
+        treatResult(LoginWithAndroidDeviceIDAsync(request), "playfab_android_login_failed", new ResultRunnable<LoginResult>() {
+            @Override
+            public void run(LoginResult result) {
+                GodotLib.calldeferred(instanceId, "playfab_android_login_succeeded", new Object[]{});
+            }
+        });
     }
 
     private static <RT> String CompileErrorsFromResult(PlayFabResult<RT> result) {
@@ -116,40 +101,11 @@ public class PlayFab extends Godot.SingletonBase {
         request.Data = new HashMap<String, String>() {{
             put(key, value);
         }};
-        final FutureTask<PlayFabResult<UpdateUserDataResult>> updateTask = UpdateUserDataAsync(request);
-
-        threadPool.submit(new Runnable() {
+        treatResult(UpdateUserDataAsync(request), "set_user_data_failed", new ResultRunnable<UpdateUserDataResult>() {
             @Override
-            public void run() {
-                updateTask.run();
-                while (!updateTask.isDone()) {
-                    try {
-                        Thread.sleep(10);
-                    } catch (InterruptedException e) {
-                        Log.d("PlayFab", "Thread interrupted");
-                        GodotLib.calldeferred(instanceId, "set_user_data_failed", new Object[]{e.toString()});
-                        return;
-                    }
-                }
-                PlayFabResult<UpdateUserDataResult> result;
-                try {
-                    result = updateTask.get();
-                } catch (Exception e) {
-                    Log.d("PlayFab", "Exception in PlayFab api call: " + e); // Did you assign your PlayFabSettings.TitleId correctly?
-                    GodotLib.calldeferred(instanceId, "set_user_data_failed", new Object[]{e.toString()});
-                    return;
-                }
-                if (result != null && result.Result != null) {
-                    GodotLib.calldeferred(instanceId, "set_user_data_succeed", new Object[]{});
-                } else if (result != null && result.Error != null) {
-                    String error = CompileErrorsFromResult(result);
-                    Log.d("PlayFab", "Error in call, here's some debug information:");
-                    Log.d("PlayFab", error);
-                    GodotLib.calldeferred(instanceId, "set_user_data_failed", new Object[]{error});
-                } else {
-                    GodotLib.calldeferred(instanceId, "get_user_data_failed", new Object[]{"Unknown error"});
-                }
-             }
+            public void run(UpdateUserDataResult result) {
+                GodotLib.calldeferred(instanceId, "set_user_data_succeed", new Object[]{});
+            }
         });
     }
 
@@ -157,43 +113,14 @@ public class PlayFab extends Godot.SingletonBase {
         GetUserDataRequest request = new GetUserDataRequest();
         request.Keys = new ArrayList<String>() {{ add(key); }};
 
-        final FutureTask<PlayFabResult<GetUserDataResult>> getTask = GetUserDataAsync(request);
-
-        threadPool.submit(new Runnable() {
+        treatResult(GetUserDataAsync(request), "get_user_data_failed", new ResultRunnable<GetUserDataResult>() {
             @Override
-            public void run() {
-                getTask.run();
-                while (!getTask.isDone()) {
-                    try {
-                        Thread.sleep(10);
-                    } catch (InterruptedException e) {
-                        Log.d("PlayFab", "Thread interrupted");
-                        GodotLib.calldeferred(instanceId, "get_user_data_failed", new Object[]{e.toString()});
-                        return;
-                    }
+            public void run(GetUserDataResult result) {
+                Dictionary data = new Dictionary();
+                for (Map.Entry<String, UserDataRecord> entry : result.Data.entrySet()) {
+                    data.put(entry.getKey(), entry.getValue().Value);
                 }
-                PlayFabResult<GetUserDataResult> result;
-                try {
-                    result = getTask.get();
-                } catch (Exception e) {
-                    Log.d("PlayFab", "Exception in PlayFab api call: " + e); // Did you assign your PlayFabSettings.TitleId correctly?
-                    GodotLib.calldeferred(instanceId, "get_user_data_failed", new Object[]{e.toString()});
-                    return;
-                }
-                if (result != null && result.Result != null) {
-                    Dictionary data = new Dictionary();
-                    for(Map.Entry<String, UserDataRecord> entry : result.Result.Data.entrySet()) {
-                        data.put(entry.getKey(), entry.getValue().Value);
-                    }
-                    GodotLib.calldeferred(instanceId, "get_user_data_succeed", new Object[]{data});
-                } else if (result != null && result.Error != null) {
-                    String error = CompileErrorsFromResult(result);
-                    Log.d("PlayFab", "Error in call, here's some debug information:");
-                    Log.d("PlayFab", error);
-                    GodotLib.calldeferred(instanceId, "get_user_data_failed", new Object[]{error});
-                } else {
-                    GodotLib.calldeferred(instanceId, "get_user_data_failed", new Object[]{"Unknown error"});
-                }
+                GodotLib.calldeferred(instanceId, "get_user_data_succeed", new Object[]{data});
             }
         });
     }
@@ -207,39 +134,10 @@ public class PlayFab extends Godot.SingletonBase {
             add(stat);
         }};
 
-        final FutureTask<PlayFabResult<UpdatePlayerStatisticsResult>> setTask = UpdatePlayerStatisticsAsync(request);
-
-        threadPool.submit(new Runnable() {
+        treatResult(UpdatePlayerStatisticsAsync(request), "set_player_statistic_failed", new ResultRunnable<UpdatePlayerStatisticsResult>() {
             @Override
-            public void run() {
-                setTask.run();
-                while (!setTask.isDone()) {
-                    try {
-                        Thread.sleep(10);
-                    } catch (InterruptedException e) {
-                        Log.d("PlayFab", "Thread interrupted");
-                        GodotLib.calldeferred(instanceId, "set_player_statistic_failed", new Object[]{e.toString()});
-                        return;
-                    }
-                }
-                PlayFabResult<UpdatePlayerStatisticsResult> result;
-                try {
-                    result = setTask.get();
-                } catch (Exception e) {
-                    Log.d("PlayFab", "Exception in PlayFab api call: " + e); // Did you assign your PlayFabSettings.TitleId correctly?
-                    GodotLib.calldeferred(instanceId, "set_player_statistic_failed", new Object[]{e.toString()});
-                    return;
-                }
-                if (result != null && result.Result != null) {
-                    GodotLib.calldeferred(instanceId, "set_player_statistic_succeeded", new Object[]{});
-                } else if (result != null && result.Error != null) {
-                    String error = CompileErrorsFromResult(result);
-                    Log.d("PlayFab", "Error in set player statistic call, here's some debug information:");
-                    Log.d("PlayFab", error);
-                    GodotLib.calldeferred(instanceId, "set_player_statistic_failed", new Object[]{error});
-                } else {
-                    GodotLib.calldeferred(instanceId, "set_player_statistic_failed", new Object[]{"Unknown error"});
-                }
+            public void run(UpdatePlayerStatisticsResult result) {
+                GodotLib.calldeferred(instanceId, "set_player_statistic_succeeded", new Object[]{});
             }
         });
     }
@@ -252,39 +150,84 @@ public class PlayFab extends Godot.SingletonBase {
 
         final FutureTask<PlayFabResult<GetPlayerStatisticsResult>> getTask = GetPlayerStatisticsAsync(request);
 
+        treatResult(getTask, "get_player_statistic_failed", new ResultRunnable<GetPlayerStatisticsResult>() {
+            @Override
+            public void run(GetPlayerStatisticsResult result) {
+                int value = result.Statistics.get(0).Value;
+                GodotLib.calldeferred(instanceId, "get_player_statistic_succeeded", new Object[]{name, value});
+            }
+        });
+    }
+
+    public void linkAndroidDeviceId(final String androidId) {
+        LinkAndroidDeviceIDRequest request = new LinkAndroidDeviceIDRequest();
+        request.AndroidDeviceId = androidId;
+        request.AndroidDevice = Build.MODEL;
+        request.OS = Build.VERSION.RELEASE;
+        request.ForceLink = true;
+
+        treatResult(LinkAndroidDeviceIDAsync(request), "link_android_device_id_failed", new ResultRunnable<LinkAndroidDeviceIDResult>() {
+            @Override
+            public void run(LinkAndroidDeviceIDResult result) {
+                GodotLib.calldeferred(instanceId, "link_android_device_id_succeeded", new Object[]{});
+            }
+        });
+    }
+
+    public void linkFacebookAccount(final String accessToken) {
+        LinkFacebookAccountRequest request = new LinkFacebookAccountRequest();
+        request.AccessToken = accessToken;
+        request.ForceLink = true;
+
+        treatResult(LinkFacebookAccountAsync(request), "link_facebook_account_failed", new ResultRunnable<LinkFacebookAccountResult>() {
+            @Override
+            public void run(LinkFacebookAccountResult result) {
+                GodotLib.calldeferred(instanceId, "link_facebook_account_succeeded", new Object[]{});
+            }
+        });
+    }
+
+    private <T> void treatResult(final FutureTask<PlayFabResult<T>> task, final String fail_method, final ResultRunnable<T> success_treatment) {
         threadPool.submit(new Runnable() {
             @Override
             public void run() {
-                getTask.run();
-                while (!getTask.isDone()) {
+                task.run();
+                Log.d("PlayFab", "running task " + fail_method);
+                while (!task.isDone()) {
                     try {
                         Thread.sleep(10);
+                        Log.d("PlayFab", "running task " + fail_method + ", sleeping thread");
+                        task.run();
                     } catch (InterruptedException e) {
                         Log.d("PlayFab", "Thread interrupted");
-                        GodotLib.calldeferred(instanceId, "get_player_statistic_failed", new Object[]{e.toString()});
+                        GodotLib.calldeferred(instanceId, fail_method, new Object[]{e.toString()});
                         return;
                     }
                 }
-                PlayFabResult<GetPlayerStatisticsResult> result;
+                PlayFabResult<T> result;
                 try {
-                    result = getTask.get();
+                    result = task.get();
                 } catch (Exception e) {
                     Log.d("PlayFab", "Exception in PlayFab api call: " + e);
-                    GodotLib.calldeferred(instanceId, "get_player_statistic_failed", new Object[]{e.toString()});
+                    GodotLib.calldeferred(instanceId, fail_method, new Object[]{e.toString()});
                     return;
                 }
                 if (result != null && result.Result != null ) {
-                    int value = result.Result.Statistics.get(0).Value;
-                    GodotLib.calldeferred(instanceId, "get_player_statistic_succeeded", new Object[]{name, value});
+                    success_treatment.run(result.Result);
                 } else if (result != null && result.Error != null) {
                     String error = CompileErrorsFromResult(result);
-                    Log.d("PlayFab", "Error in get player statistic call, here's some debug information:");
+                    Log.d("PlayFab", "Error in " + fail_method + " call, here's some debug information:");
                     Log.d("PlayFab", error);
-                    GodotLib.calldeferred(instanceId, "get_player_statistic_failed", new Object[]{error});
+                    GodotLib.calldeferred(instanceId, fail_method, new Object[]{error});
                 } else {
-                    GodotLib.calldeferred(instanceId, "get_player_statistic_failed", new Object[]{"Unknown error"});
+                    GodotLib.calldeferred(instanceId, fail_method, new Object[]{"Unknown error"});
                 }
             }
         });
     }
+
+    abstract class ResultRunnable<T> {
+        public abstract void run(T result);
+    }
+
 }
