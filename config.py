c78de0cebbcc4f116a0fdcbf3a89dcd7f1dd1809
Jairo Honorio <registrofx@gmail.com>
2019-07-22 17:27:57 -0300

    Fix config.py according to latest scons

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 
 def can_build(env, platform):
-    return platform == "android" or platform == "iphone"
+    return platform=="android" or platform=="iphone"
 
 def configure(env):
     if (env['platform'] == 'android'):
@@ -9,6 +9,5 @@ def configure(env):
         env.android_add_dependency("implementation 'com.android.support:multidex:1.0.3'")
         env.android_add_java_dir("android/src/")
     if env['platform'] == "iphone":
-        #env.Append(FRAMEWORKPATH=['modules/GodotPlayFab/ios/lib'])
-        #env.Append(LINKFLAGS=['-ObjC', '-framework', 'FBSDKCoreKit', '-framework', 'FBSDKLoginKit', '-framework', 'FBSDKShareKit', '-framework', 'Bolts'])
-        pass
+        env.Append(FRAMEWORKPATH=['ios/lib'])
+        env.Append(LINKFLAGS=['-ObjC'])#, '-framework', 'FBSDKCoreKit', '-framework', 'FBSDKLoginKit', '-framework', 'FBSDKShareKit', '-framework', 'Bolts'])
